a9e67671554354e68ca15e2afc39c2b5c0da71aa
metzwijaya <andre19961@gmail.com>
2018-10-30 21:48:12 +0700

    - detail team - add favorite team

--- app/src/main/java/com/example/wijaya_pc/footballapps/feature/team/DetailTeamActivity.kt
+++ app/src/main/java/com/example/wijaya_pc/footballapps/feature/team/DetailTeamActivity.kt
@@ -1,14 +1,25 @@
 package com.example.wijaya_pc.footballapps.feature.team
 
+import android.R
+import android.database.sqlite.SQLiteConstraintException
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.example.wijaya_pc.footballapps.R.drawable.ic_add_to_favorites
+import com.example.wijaya_pc.footballapps.R.drawable.ic_added_to_favorites
 import com.example.wijaya_pc.footballapps.R.id.*
+import com.example.wijaya_pc.footballapps.R.menu.detail_menu
 import com.example.wijaya_pc.footballapps.api.ApiRepository
+import com.example.wijaya_pc.footballapps.database.database
+import com.example.wijaya_pc.footballapps.database.databaseTeam
 import com.example.wijaya_pc.footballapps.invisible
+import com.example.wijaya_pc.footballapps.model.FavoriteTeams
 import com.example.wijaya_pc.footballapps.model.Team
 import com.example.wijaya_pc.footballapps.presenter.DetailTeamPresenter
 import com.example.wijaya_pc.footballapps.ui.DetailTeamUI
@@ -16,6 +27,12 @@ import com.example.wijaya_pc.footballapps.view.DetailTeamView
 import com.example.wijaya_pc.footballapps.visible
 import com.google.gson.Gson
 import com.squareup.picasso.Picasso
+import org.jetbrains.anko.ctx
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.delete
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.find
 import org.jetbrains.anko.setContentView
 import org.jetbrains.anko.support.v4.onRefresh
@@ -24,6 +41,8 @@ class DetailTeamActivity : AppCompatActivity(), DetailTeamView {
 
     private lateinit var swipeRefresh : SwipeRefreshLayout
     private lateinit var progressBar : ProgressBar
+    private var menuItem: Menu? = null
+    private var isFavorite: Boolean = false
 
     private lateinit var teamBadge: ImageView
     private lateinit var teamName: TextView
@@ -55,6 +74,8 @@ class DetailTeamActivity : AppCompatActivity(), DetailTeamView {
         supportActionBar?.title = "Team Detail"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        favoriteState()
+
         val request = ApiRepository()
         val gson = Gson()
         presenter = DetailTeamPresenter(this, request, gson)
@@ -64,6 +85,48 @@ class DetailTeamActivity : AppCompatActivity(), DetailTeamView {
             presenter.getTeamDetail(id)
         }
 
+    }
+
+    // membuat menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(detail_menu, menu)
+        menuItem = menu
+        setFavorite()
+        return true
+    }
+
+    // apa yg dilakukan ketika menu diklik
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.home -> {
+                finish()
+                true
+            }
+            add_to_favorite -> {
+
+                if (isFavorite) {
+                    presenter.removeFromFavoriteTeam(ctx, teams.teamId)
+                    snackbar(swipeRefresh, "Removed from FavoriteTeams").show()
+                }
+                else {
+                    presenter.addToFavoriteTeam(ctx,
+                        teams.teamId,
+                        teams.teamName,
+                        teams.teamBadge,
+                        teams.teamFormedYear,
+                        teams.teamStadium,
+                        teams.teamDescription
+                    )
+                    snackbar(swipeRefresh, "Added to FavoriteTeams").show()
+                }
+
+                isFavorite = !isFavorite
+                setFavorite()
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun showLoading() {
@@ -77,7 +140,10 @@ class DetailTeamActivity : AppCompatActivity(), DetailTeamView {
     override fun showTeamDetail(data: List<Team>) {
         teams = Team(data[0].teamId,
             data[0].teamName,
-            data[0].teamBadge)
+            data[0].teamBadge,
+            data[0].teamFormedYear,
+            data[0].teamStadium,
+            data[0].teamDescription)
 
         swipeRefresh.isRefreshing = false
         Picasso.get().load(data[0].teamBadge).into(teamBadge)
@@ -85,5 +151,24 @@ class DetailTeamActivity : AppCompatActivity(), DetailTeamView {
         teamDescription.text = data[0].teamDescription
         teamFormedYear.text = data[0].teamFormedYear
         teamStadium.text = data[0].teamStadium
+    }
+
+    // fungsi untuk menandai tim yang favorite
+    private fun setFavorite() {
+        if (isFavorite)
+            menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, ic_added_to_favorites)
+        else
+            menuItem?.getItem(0)?.icon = ContextCompat.getDrawable(this, ic_add_to_favorites)
+    }
+
+
+    private fun favoriteState(){
+        databaseTeam.use {
+            val result = select(FavoriteTeams.TABLE_FAVORITE_TEAMS)
+                .whereArgs("(TEAM_ID = {id})",
+                    "id" to id)
+            val favorite = result.parseList(classParser<FavoriteTeams>())
+            if (!favorite.isEmpty()) isFavorite = true
+        }
     }
 }
--- app/src/main/java/com/example/wijaya_pc/footballapps/presenter/DetailTeamPresenter.kt
+++ app/src/main/java/com/example/wijaya_pc/footballapps/presenter/DetailTeamPresenter.kt
@@ -1,14 +1,22 @@
 package com.example.wijaya_pc.footballapps.presenter
 
+import android.content.Context
+import android.database.sqlite.SQLiteConstraintException
+import android.util.Log
 import com.example.wijaya_pc.footballapps.api.ApiRepository
 import com.example.wijaya_pc.footballapps.api.TheSportDBApi
 import com.example.wijaya_pc.footballapps.coroutine.CoroutineContextProvider
+import com.example.wijaya_pc.footballapps.database.databaseTeam
+import com.example.wijaya_pc.footballapps.model.FavoriteTeams
 import com.example.wijaya_pc.footballapps.model.TeamResponse
 import com.example.wijaya_pc.footballapps.view.DetailTeamView
 import com.google.gson.Gson
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.coroutines.experimental.bg
+import org.jetbrains.anko.db.delete
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.design.snackbar
 
 class DetailTeamPresenter(private val view: DetailTeamView,
                           private val apiRepository: ApiRepository,
@@ -29,6 +37,32 @@ class DetailTeamPresenter(private val view: DetailTeamView,
             }
             view.showTeamDetail(data.await().teams)
             view.hideLoading()
+        }
+    }
+
+    fun removeFromFavoriteTeam(context: Context, teamId: String?) {
+        try {
+            context.databaseTeam.use {
+                delete(FavoriteTeams.TABLE_FAVORITE_TEAMS, "(TEAM_ID = {id})", "id" to "$teamId")
+            }
+        } catch (e: SQLiteConstraintException){
+            Log.d("failed delete fav", e.localizedMessage)
+        }
+    }
+
+    fun addToFavoriteTeam(context: Context, teamId: String?, teamName: String?, teamBadge: String?, teamFormedYear: String?, teamStadium: String?, teamDescription: String?) {
+        try {
+            context.databaseTeam.use {
+                insert(FavoriteTeams.TABLE_FAVORITE_TEAMS,
+                    FavoriteTeams.TEAM_ID to teamId,
+                    FavoriteTeams.TEAM_NAME to teamName,
+                    FavoriteTeams.TEAM_BADGE to teamBadge,
+                    FavoriteTeams.TEAM_FORMED_YEAR to teamFormedYear,
+                    FavoriteTeams.TEAM_STADIUM to teamStadium,
+                    FavoriteTeams.TEAM_DESCRIPTION to teamDescription)
+            }
+        } catch (e: SQLiteConstraintException){
+            Log.d("failed add fav", e.localizedMessage)
         }
     }
 
